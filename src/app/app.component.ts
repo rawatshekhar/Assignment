2c77d5b1a8edc7e0f8f0ac5d5af39025c2f07a0a
Rohit Kapoor <rkapur@mag-corp.com>
2021-11-01 18:51:39 +0530

    Assignment Committed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Assignment';
+  hide = true;
+  hide1 = true;
+  registrationForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private _snackBar: MatSnackBar
+  ) {
+    this.registrationForm = this.fb.group({
+      email: ['', [Validators.required,Validators.email]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
+      contact: ['',[Validators.required,Validators.minLength(10),
+                  Validators.maxLength(10),
+                  Validators.pattern("^[0-9]*$")]
+                ],
+      gender: [''],
+      active_status: [0],
+      hobbies: [''],
+      city: ['', [Validators.required]],
+      file: ['', ],
+    });
+
+  }
+  get f1() { return this.registrationForm.controls; }
+  getEmailErrorMessage() {
+    if (this.registrationForm.controls.email.hasError('required')) {
+      return 'You must enter an Email';
+    }
+    return this.registrationForm.controls.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  getPhoneErrorMessage() {
+    if (this.registrationForm.controls.contact.hasError('required')) {
+      return 'You must enter a Contact Number';
+    }
+    return this.registrationForm.controls.contact.hasError('contact') ? 'Not a valid Contact' : '';
+  }
+  save(formData){
+    let message="Form Saved";
+    console.log("Form Value",formData);
+    this.registrationForm.reset();
+    this._snackBar.open(message, 'Dismiss');
+  }
+  // uploadFile(){
+
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import {MatRadioModule} from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -10,7 +32,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
